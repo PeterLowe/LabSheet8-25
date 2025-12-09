f996d05f037256a1015865afd57e1ab0839cf9e8
Peter Lowe <lowep@itcarlow.ie>
2025-12-09 09:38:32 +0000

    initial code

---
+++ Labsheet8/Q1.cpp
@@ -0,0 +1,18 @@
+
+
+// name
+// estimated time:
+// actual time:
+// description:
+
+// comments for Pete
+
+#include <iostream>
+
+int main()
+{
+
+	std::cout << "question 1" << std::endl;
+
+	return 1;
+}
